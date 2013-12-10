72c4600a7220d5b788fee6be78748dc369b8d783
zzz6519003 <zhengzhongzhao@gmail.com>
2013-12-10 16:25:02 +0800

    fix bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -19,7 +19,19 @@ class StaticPagesController < ApplicationController
     token = params[:token]
     current_user ||= User.find_by_remember_token(token)
     renren = params[:renren_id]
-    current_user.update_attribute(:renren_id,renren)
+    #current_user.updatee_attribute(:renren_id,renren, :remember_token, current_user.remember_token)
+          current_user.update_column :renren_id, renren
+
+  end
+
+  def match_mobile
+    matched_user = []
+    renren_id_array = params[:params]
+    renren_id_array.each do |uid|
+      user = User.find_by_renren_id uid
+      matched_user.append user
+    end
+    render :json => matched_user
 
   end
 
